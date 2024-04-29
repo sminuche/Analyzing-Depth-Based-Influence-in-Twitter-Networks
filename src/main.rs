6369bde3661faf921651ff50a6094fe954239705
Samantha Minuche <sminuche@bu.edu>
2024-04-29 19:01:22 -0400

    fifth commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::{collections::{HashMap, HashSet}, fs};
 use rand::prelude::SliceRandom;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+
 
 #[derive(Debug)]
 struct Graph {
@@ -8,25 +12,33 @@ struct Graph {
 
 impl Graph {
     fn new() -> Self {
-        Self { adjacency_list: HashMap::new() }
+        Self {
+            adjacency_list: HashMap::new(),
+        }
     }
 
     fn add_edge(&mut self, node1: String, node2: String) {
-        self.adjacency_list.entry(node1.clone()).or_insert_with(HashSet::new).insert(node2.clone());
-        self.adjacency_list.entry(node2).or_insert_with(HashSet::new).insert(node1);
+        self.adjacency_list
+            .entry(node1.clone())
+            .or_insert_with(HashSet::new)
+            .insert(node2.clone());
+        self.adjacency_list
+            .entry(node2)
+            .or_insert_with(HashSet::new)
+            .insert(node1);
     }
 
     fn breadth_first_search(&self, node: &str, depth: usize) -> HashSet<String> {
         let mut visited = HashSet::new();
         let mut queue = Vec::new();
         queue.push((node.to_string(), 0));
-    
+
         while let Some((current_node, current_depth)) = queue.pop() {
             if visited.contains(&current_node) || current_depth >= depth {
                 continue;
             }
             visited.insert(current_node.clone());
-    
+
             if let Some(neighbors) = self.adjacency_list.get(&current_node) {
                 for neighbor in neighbors {
                     if !visited.contains(neighbor) {
@@ -41,9 +53,16 @@ impl Graph {
     fn sample_random_nodes(&self, num_nodes: usize) -> Vec<String> {
         let mut rng = rand::thread_rng();
         let keys: Vec<_> = self.adjacency_list.keys().cloned().collect();
-        let mut shuffled_keys: Vec<String> = keys.choose_multiple(&mut rng, num_nodes).cloned().collect();
+        let mut shuffled_keys: Vec<String> =
+            keys.choose_multiple(&mut rng, num_nodes).cloned().collect();
         shuffled_keys.shuffle(&mut rng);
         shuffled_keys
+    }
+}
+
+fn print_best_nodes(node_vec: Vec<(usize, String, f64)>) {
+    for (depth, node, prop) in node_vec.iter(){
+        println!("For depth {}, the most popular node is {}, covering {}% of all users", depth, node, prop * 100.0);
     }
 }
 
@@ -58,47 +77,43 @@ fn load_dataset(filename: &str) -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let total_nodes = graph.adjacency_list.len(); 
+    let total_nodes = graph.adjacency_list.len();
     let batch_size = 100;
-    
+
     let mut distance_map: HashMap<&String, Vec<f64>> = HashMap::new();
+    let mut max_vector = Vec::new();
     let random_nodes = graph.sample_random_nodes(batch_size);
 
-    for depth in 2..=4 {
-        //let (mut total_overlap, mut total_followers) = (0, 0);
-        println!("{:?}", distance_map);
+    for depth in 1..=6 {
+        //println!("{:?}", distance_map);
 
         for node in &random_nodes {
-            //let direct_friends = graph.adjacency_list.get(&node).unwrap_or(&HashSet::new()).clone();
             let friends_of_friends = graph.breadth_first_search(&node, depth);
-            let proportion =  friends_of_friends.len() as f64/total_nodes as f64;
-            println!("{}", proportion);
+            let proportion = friends_of_friends.len() as f64 / total_nodes as f64;
+            //println!("{}", proportion);
             if let Some(proportions) = distance_map.get_mut(node) {
                 proportions.push(proportion);
-                
             } else {
                 let initialized_proportions = vec![proportion];
                 distance_map.insert(node, initialized_proportions);
-
             }
-
-
-            //println!("{:?}",friends_of_friends);
-
-            //total_overlap += direct_friends.intersection(&friends_of_friends).count();
-            //println!("total overlap: {}", total_overlap);
-            //total_followers += friends_of_friends.len();
-            //println!("total followers: {}", total_followers);
+        }
+        let mut max_proportion = 0.0;
+        let mut max_node = String::new();
+        for (node, proportions) in distance_map.iter() {
+            for proportion in proportions {
+                if *proportion > max_proportion {
+                    max_proportion = *proportion;
+                    max_node = node.to_string();
+                }
+            }
 
         }
 
-    //let average_overlap = if total_followers > 0 { total_overlap as f64 / total_followers as f64 } else { 0.0 };
-    //println!("Average overlap at depth {}: {:.2}", depth, average_overlap);
+        max_vector.push((depth, max_node, max_proportion));
     }
 
-    for(key, value) in &distance_map {
-        println!("Node: {}, Proportions: {:?}", key, value)
-    }
+    print_best_nodes(max_vector);
     Ok(())
 }
 
